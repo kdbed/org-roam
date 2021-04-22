70f803af18bbefa145ddb9af5a6e96eaf081d22d
Kyle Bednar <kbednar3@kent.edu>
2021-04-21 21:53:17 -0400

    update

---
+++ .auctex-auto/refs.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "bird_gibbons_2020"
+    "gopalakrishnan2019automata"
+    "haber2019identity"
+    "kurt2018get"
+    "sheppard2016genetic"
+    "troncone2019cybersecurity"))
+ :bibtex)
+
